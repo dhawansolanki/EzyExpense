4b75f92cef8e5b71bcf390d9cb0b2d7db26aaf76
Dhawan Solanki <dhawansolanki2103@gmail.com>
2024-05-14 21:06:09 +0530

    Initial Commit

---
+++ src/components/header.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a
+            href="/"
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          >
+            {/* <Image src="/ezy.png" alt="Logo" width={100} height={200} /> */}
+            {/* <span className="ml-3 text-xl">EzyExpense</span> */}
+          </a>
+          {/* <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <a href="/" className="mr-5 hover:text-gray-900">
+              Home
+            </a>
+            <a href="/about" className="mr-5 hover:text-gray-900">
+              About
+            </a>
+            <a href="/contact" className="mr-5 hover:text-gray-900">
+              Contact
+            </a>
+          </nav> */}
+          {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Signup
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button> */}
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,29 @@
+export default function Hero() {
+  return (
+    <>
+      <div className="flex justify-center ">
+        <div className="flex flex-col justify-center">
+          <div className="flex flex-col max-w-7xl justify-center items-center p-2 mt-10">
+            <div className="flex flex-col w-[90%] md:w-3/4 text-center items-center justify-center space-y-3">
+              <div className="text-5xl font-bold ">
+                Manage Your Expenses Easily With EzyExpense
+              </div>
+              <br />
+              <div className="text-xl font-semibold text-black-400 ">
+                We are providing easiest way to to manage expenses, Get a full
+                view so you know where to save.
+              </div>
+            </div>
+            <div className="w-3/4 h-60 md:h-96 my-4  rounded-lg overflow-hidden">
+              <img
+                src="https://source.unsplash.com/500x400/?expenses,money,savemoney"
+                className="h-full w-full"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
